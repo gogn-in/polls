e26344a074f4a014a2a5b0799e1d0aae97fd3d26
Páll Hilmarsson <pallih@kaninka.net>
2016-01-08 14:05:46 +0000

    Test that datapackage has data datafiles
    referenced in datapackage.json

--- tests/test_datapackage_json.py
+++ tests/test_datapackage_json.py
@@ -3,17 +3,33 @@
 import datapackage_validate
 import json
 import os
+from nose.tools import nottest
+
 import unittest
 
 
 class TestValidDatapackage(unittest.TestCase):
 
-    def test_datapackage_json_validates(self):
+    @nottest
+    def find_datapackage_files(self):
         for root, dirs, files in os.walk('.'):
             for filename in files:
                 if filename == "datapackage.json":
-                    f = open(os.path.join(root, filename))
-                    datapackagejson = json.load(f)
-                    datapackage_validate.validate(datapackagejson)
+                    yield (root, filename)
+
+    def test_datapackage_json_validates(self):
+        for root, filename in self.find_datapackage_files():
+            with open(os.path.join(root, filename)) as f:
+                datapackagejson = json.load(f)
+                datapackage_validate.validate(datapackagejson)
+
+    def test_datapackage_has_data_files(self):
+        for root, filename in self.find_datapackage_files():
+            with open(os.path.join(root, filename)) as f:
+                datapackagejson = json.load(f)
+                resources = datapackagejson["resources"]
+                for resource in resources:
+                    assert(os.path.isfile(os.path.join(root, resource["path"])))
+
 
 
